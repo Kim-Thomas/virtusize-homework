32be7cf970b642afb3be5668a45b5d916bf1ca28
Thomas Kim <thomas@meow.fr>
2019-07-08 15:04:35 +0900

    Fix: Commenting and removing extralines for Font-awesome.

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/'
+// Font-awesome
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret, faSocks, faQuestionCircle, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import { faUserSecret, faSocks, faQuestionCircle, faSignOutAlt, faPen } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-
-library.add(faUserSecret, faSocks, faQuestionCircle, faSignOutAlt)
+library.add(faUserSecret, faSocks, faQuestionCircle, faSignOutAlt, faPen)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 

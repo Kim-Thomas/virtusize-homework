e8db04892422d3a1fb2f50753ed8c9d0007aaad6
Thomas Kim <thomas@meow.fr>
2019-07-09 11:37:46 +0900

    Moved the dashboard sidebar into its own component.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,38 +1,6 @@
 <template>
   <div class="dashboard">
-    <div class="sidebar">
-      <div class="site-branding">
-        <img class="site-branding__logo" alt="Virtusize Logo" :src="LogoWithText">
-      </div>
-      <div class="content">
-        <h3 class="sidebar-title">Dashboard</h3>
-        <ul>
-          <li>
-            <router-link to="/dashboard/account-details">
-              <font-awesome-icon icon="user-secret" /> Account Details
-            </router-link>
-          </li>
-          <li>
-            <router-link to="/dashboard/my-wardrobe">
-              <font-awesome-icon icon="socks" /> My Wardrobe
-            </router-link>
-          </li>
-        </ul>
-        <h3 class="sidebar-title">Others</h3>
-        <ul>
-          <li>
-            <router-link to="/dashboard/about">
-              <font-awesome-icon icon="question-circle" /> About
-            </router-link>
-          </li>
-          <li>
-            <a href="" class='log-out-link' v-on:click.prevent="logOut()">
-              <font-awesome-icon icon="sign-out-alt" /> Log Out
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <DashboardSidebar />
     <div class="dashboard-content">
       <router-view></router-view>
     </div>
@@ -40,20 +8,11 @@
 </template>
 
 <script>
-import LogoWithText from '@/assets/brand/logo-w-text.svg';
+import DashboardSidebar from '@/components/macro/DashboardSidebar.vue';
 
 export default {
-  computed: {
-    LogoWithText() { return LogoWithText }
-  },
-  methods: {
-    logOut: function() {
-      let app = this;
-      this.$store.dispatch('logOut')
-        .then(function(result) {
-          app.$router.push({ path: '/' })
-        });
-    }
+  components: {
+    DashboardSidebar
   },
   created: function() {
     let app = this;
@@ -71,95 +30,5 @@ export default {
   height: 100%;
   display: grid;
   grid-template-columns: 320px 1fr;
-
-  .sidebar {
-    border-right: .5px solid rgba(0,0,0,.2);
-    position: relative;
-    background: white;
-    display: flex;
-    align-items: center;
-    z-index: 9;
-
-    .site-branding {
-      position: absolute;
-      top: 30px;
-      left: 30px;
-
-      &__logo {
-        height: 25px;
-      }
-    }
-
-    .content {
-      width: 100%;
-      padding: 0 30px;
-      box-sizing: border-box;
-
-      .sidebar-title {
-        color: #16C6B9;
-        font-weight: 900;
-        font-size: .8em;
-        margin-top: 20px;
-        margin-bottom: 20px;
-        text-transform: uppercase;
-        opacity: 1;
-      }
-
-      ul {
-        padding: 0;
-        margin: 0;
-
-        li {          
-          position: relative;
-          list-style: none;
-
-          a {
-            display: block;
-            text-decoration: none;
-            color: inherit;
-            font-size: 1em;
-            font-weight: 600;
-            padding: 15px 0;
-            opacity: .6;
-
-            &.log-out-link {
-              color: #e74c3c;
-            }
-
-            &:hover {
-              &::after {
-                content: "";
-                position: absolute;
-                top: 0;
-                left: calc(~'320px - 30px - 4px');
-                height: 100%;
-                width: 4px;
-                background: rgba(22, 198, 186, 0.3);
-              }   
-            }
-
-            &.router-link-active {
-              opacity: .8;
-
-              &::after {
-                content: "";
-                position: absolute;
-                top: 0;
-                left: calc(~'320px - 30px - 4px');
-                height: 100%;
-                width: 4px;
-                background: #16C6B9;
-              }
-            }
-
-            svg {
-              width: 20px;
-              margin-right: 10px;
-            }
-          }
-        }
-      }
-    }
-  }
 }
 </style>

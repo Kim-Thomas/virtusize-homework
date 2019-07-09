1b7a8804613fe93715018e14ee87abaad70de16c
Thomas Kim <thomas@meow.fr>
2019-07-09 10:11:39 +0900

    Add: placehodler for Wardrobe page.

--- src/views/dashboard/Wardrobe.vue
+++ src/views/dashboard/Wardrobe.vue
@@ -1,5 +1,43 @@
 <template>
-  <div>
-    Wardrobe
+  <div class="wardrobe">
+    <img :src="Monitor" class="placeholder-image">
+    <h1 class="placeholder-title">Working on it...</h1>
   </div>
 </template>
+
+<script>
+import Monitor from '@/assets/images/monitor.svg'
+
+export default {
+  computed: {
+    Monitor() {
+      return Monitor;
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.wardrobe {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  .placeholder-image {
+    max-width: 60%;
+    max-height: 60%;
+  }
+
+  .placeholder-title {
+    margin-top: 60px;
+    font-size: 2em;
+    font-weight: 400;
+    font-style: italic;
+    opacity: .2;
+  }
+}
+</style>
+

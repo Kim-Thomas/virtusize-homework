dcd76502b7fe096b9bb7b5eadee81147220b36ee
Thomas Kim <thomas@meow.fr>
2019-07-10 11:46:56 +0900

    Fix: updated ESLint rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,10 +8,16 @@ module.exports = {
     '@vue/standard'
   ],
   rules: {
+    'camelcase': 'off',
+    'no-useless-escape': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
+  },
+  globals: {
+    '$': false,
+    'setTimeout': false
   }
 }
